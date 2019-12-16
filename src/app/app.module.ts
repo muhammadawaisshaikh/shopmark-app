8714147906efc598a0149ea6b77d603480389653
Muhammad Awais <awaisshaikh94@gmail.com>
2019-12-16 17:54:37 +0500

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { LoaderComponent } from './shared/loader/loader.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { SidemenuComponent } from './layout/sidemenu/sidemenu.component';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ListingComponent } from './shared/listing/listing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoaderComponent,
     HeaderComponent,
-    SidemenuComponent
+    SidemenuComponent,
+    HomeComponent,
+    CartComponent,
+    ProductDetailsComponent,
+    ProductsComponent,
+    ListingComponent
   ],
   imports: [
     BrowserModule,
